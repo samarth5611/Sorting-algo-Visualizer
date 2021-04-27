fc387525d2b4175a3d618d32d407299b181e471e
sammy <samarthajadhao5611@gmail.com>
2021-04-27 19:24:34 +0530

    Bubble Sort completed

--- src/components/sorting.jsx
+++ src/components/sorting.jsx
@@ -1,34 +1,89 @@
-import React, { useState } from "react";
-// import Print from './print'
-
+import React, { Component } from "react";
 import "./sorting.css";
-function Sorting() {
-  const [Arr, setArr] = useState([null]);
 
-  function init() {
-    const arr = [10];
-    for (let i = 0; i < 11; i++) {
-      arr[i] = rand(5, 100);
-    }
-    setArr(arr);
-    console.log(Arr);
+const SIZE = 150;
+
+export default class sorting extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      array: [],
+    };
   }
 
-  return (
-    <div className="container">
-      <button onClick={init}>Genrate Array</button>
-      <div className="array-case">
-        {Arr.map((x, idx) => (
-          <div className="array-bar" key={idx}>
-            {" "}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+  componentDidMount() {
+    this.init();
+  }
 
-export default Sorting;
+  init() {
+    const a = [];
+    for (let i = 0; i < SIZE; i++) {
+      a.push(rand(10, 400));
+    }
+    this.setState({ array: a });
+    console.log("init");
+    console.log(this.state);
+  }
+
+  bubbleSort() {
+    console.log("Bubble Sort");
+    var a = this.state.array,
+      idx = 1;
+    const solve = setInterval(() => {
+      if (this.is_sorted(a)) {
+        clearInterval(solve);
+      }
+      if (idx >= a.length) {
+        idx = 1;
+      }
+
+      if (a[idx] < a[idx - 1]) {
+        a[idx] = a[idx] ^ a[idx - 1];
+        a[idx - 1] = a[idx] ^ a[idx - 1];
+        a[idx] = a[idx] ^ a[idx - 1];
+        this.setState({ array: a });
+      }
+      idx++;
+    }, 0);
+  }
+
+  is_sorted(arr) {
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) return false;
+    }
+    return true;
+  }
+
+  render() {
+    const { array } = this.state;
+    console.log(array);
+
+    return (
+      <div className="container">
+        <div className="array-case">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <div className="buttons">
+          <button className="btn" onClick={() => this.bubbleSort()}>
+            {" "}
+            BubbleSort{" "}
+          </button>
+          <button className="btn" onClick={() => this.mergeSort()}>
+            {" "}
+            MergeSort{" "}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 function rand(l, r) {
   return Math.floor(Math.random() * (r - l + 1) + l);
